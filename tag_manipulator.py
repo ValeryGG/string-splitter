dda310357376e2ce13be8b9eef0e278ed090008f
Galina Gasparyan <galina.gasparyan29.08@gmail.com>
2021-07-29 16:29:22 +0100

    cut4 - test with a comma and string - array of one

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,11 +1,16 @@
 import re
 
 class TagManipulator():    
-    def parse_string(self, tags, regex=""):
+    def parse_string(self, tags, regex=",\\ *"):
         result = []
 
         tempResult = re.split( regex, tags )
+        
         if( len(tempResult[0]) > 0 ):
-            result = tempResult  
+            result=tempResult
+        else:
+            for unit in tempResult:
+                if not unit=="":
+                    result.append(unit) 
 
         return result
